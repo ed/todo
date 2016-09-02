4e5d43e9cd0c47fb7992c42b78fc78342163391e
edward <ecguo@ucdavis.edu>
2016-09-01 17:30:22 -0700

    added on click events to todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,28 +4,18 @@ var ReactPropTypes = React.PropTypes;
 class Todo extends Component {
     propTypes: {
         todo: ReactPropTypes.object,
+        handleClick: ReactPropTypes.func, 
         idx: ReactPropTypes.Number
     };
-
-
-    constructor(props) {
-        super(props);
-        this._handleClick = this._handleClick.bind(this);
-
-    };
-
-    _handleClick(e) {
-        e.preventDefault();
-        console.log(e.target.id);
-    }
 
     render (){
         var todo = this.props.todo;
         var idx = this.props.idx;
+        var handleClick = this.props.handleClick;
         return (
             <li className="todo-list-item">
                 <h5 className="todo-name">
-                    <a href="#" id={idx} onClick={(e) => this._handleClick(e)}>
+                    <a href="#" id={idx} onClick={(e) => handleClick(e)}>
                         {todo} 
                     </a>
                 </h5>
--- src/components/TodoSection.jsx
+++ src/components/TodoSection.jsx
@@ -8,16 +8,22 @@ class TodoSection extends Component {
 
     constructor(props) {
         super(props);
+        const { tasks, actions } = this.props;
+        this._handleClick = this._handleClick.bind(this);
     };
+
+    _handleClick(e) {
+        e.preventDefault();
+        console.log(this.props.tasks.get(e.target.id));
+    }
 
 
     render() {
-        const { tasks, actions } = this.props;
         return (
             <div className="todo-section">
                 <ul className="todo-list" ref="todoList">
-                    {tasks.entrySeq().map( ([key, value]) =>
-                    <Todo key={value.id} todo={value.obj} idx={key} />)}
+                    {this.props.tasks.entrySeq().map( ([key, value]) =>
+                    <Todo key={value.id} todo={value.obj} idx={key} handleClick={this._handleClick} />)}
                 </ul>
             </div>
         );
